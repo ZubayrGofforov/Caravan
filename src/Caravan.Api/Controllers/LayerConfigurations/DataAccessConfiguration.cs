855fadc55bb933339cde49b650e85f3703e5f398
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-01-05 20:53:55 +0500

    Add Database connection

---
+++ src/Caravan.Api/Controllers/LayerConfigurations/DataAccessConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Caravan.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caravan.Api.Controllers.LayerConfigurations
+{
+    public static class DataAccessConfiguration
+    {
+        public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+        {
+            string connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
