4dc4d01bb4fc224705f18f53290b83b3a67fa853
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-01-07 03:08:29 +0500

    controller order

---
+++ src/Caravan.Api/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Caravan.Service.Dtos;
+using Caravan.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caravan.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _service;
+        public OrderController(IOrderService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm] OrderCreateDto dto)
+            => Ok(await _service.CreateAsync(dto));
+        public async Task<IActionResult> GetByIdAsync(long id)
+            => Ok(await _service.GetAsync(id));
+        public async Task<IActionResult> DeleteAsync(long id)
+            => Ok( await _service.DeleteAsync(id));
+        public async  UpdateAsync(long id, [FromForm] OrderCreateDto dto)
+            => Ok(await _service.UpdateAsync(id,dto));
+    }
+}
