a1c515daa118741d27f25a4558deed6343ad971b
Normadjon <gofforovnormadjon@gmail.com>
2023-01-06 10:10:26 +0500

    Repository create

---
+++ src/Caravan.DataAccess/Repositories/Comman/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Caravan.DataAccess.DbContexts;
+using Caravan.DataAccess.Interfaces.Comman;
+using Caravan.Domain.Comman;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan.DataAccess.Repositories.Comman
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected AppDbContext _dbContext;
+        protected DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public virtual void Add(T entity) => _dbSet.Add(entity);
+
+        public virtual void Delete(long id)
+        {
+            var entity = _dbSet.Find(id);
+            if(entity is not null)
+                _dbSet.Remove(entity);
+        }
+
+        public virtual async Task<T?> FindByIdAsync(long id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.FirstOrDefaultAsync(expression);
+        }
+
+        public virtual void Update(long id, T entity)
+        {
+            entity.Id = id;
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ src/Caravan.DataAccess/Repositories/Comman/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Caravan.DataAccess.DbContexts;
+using Caravan.DataAccess.Interfaces;
+using Caravan.DataAccess.Interfaces.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan.DataAccess.Repositories.Comman
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+        public IAdministratorRepository Administrators { get; }
+
+        public IOrderRepository Orders { get; }
+
+        public ITruckRepository Trucks { get; }
+
+        public IUserRepository Users { get; }
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+            
+            Administrators = new AdministratorRepository(appDbContext);
+
+            Orders = new OrderRepository(appDbContext);
+
+            Trucks = new TruckRepository(appDbContext);
+
+            Users = new UserRepository(appDbContext);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
