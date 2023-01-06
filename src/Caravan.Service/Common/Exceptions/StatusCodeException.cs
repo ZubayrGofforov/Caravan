f056883ecf5cb374e0ae0c3a084b597c362e9e6b
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-01-06 11:50:29 +0500

    exceptions

---
+++ src/Caravan.Service/Common/Exceptions/StatusCodeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan.Service.Common.Exceptions
+{
+    public class StatusCodeException : Exception
+    {
+        public HttpStatusCode statusCode { get; set; }
+
+        public StatusCodeException()
+        {
+
+        }
+        public StatusCodeException(HttpStatusCode statusCode, string message) :
+            base(message)
+        {
+            this.statusCode = statusCode;
+        }
+
+    }
+}
