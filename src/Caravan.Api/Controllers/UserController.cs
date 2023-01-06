bff333da170784a7524de8112f2052f0590ed3f1
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-01-07 04:16:53 +0500

    detached

---
+++ src/Caravan.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Caravan.Service.Dtos.Accounts;
+using Caravan.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caravan.Api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService service;
+        private readonly IAccountService accountService;
+        public UserController(IUserService service, IAccountService accountservice)
+        {
+            this.service = service;
+            this.accountService = accountservice;
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetAsync(long id)
+            => Ok(await service.GetAsync(id));
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+            => Ok(await service.DeleteAsync(id));
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(long id, [FromBody] AccountRegisterDto dto)
+            => Ok(await service.UpdateAsync(id, dto));
+    }
+}
