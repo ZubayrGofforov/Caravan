976afad49188c08a5851a5a11dd72a44c80fa0db
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-01-06 23:49:48 +0500

    update domain layer and Migration

--- src/Caravan.Domain/Entities/Truck.cs
+++ src/Caravan.Domain/Entities/Truck.cs
@@ -21,6 +21,9 @@ namespace Caravan.Domain.Entities
 
         public string TruckNumber { get; set; } = string.Empty;
 
+        public long UserId { get; set; }
+        public virtual User Users { get; set; } = default!;
+
         public long LocationId { get; set; }
         public virtual Location TruckLocation { get; set; } = default!;
     }
