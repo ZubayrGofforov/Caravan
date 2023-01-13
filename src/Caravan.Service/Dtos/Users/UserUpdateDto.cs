2527a391ce6aacfc6c7bea426920950195c8be69
Normadjon <gofforovnormadjon@gmail.com>
2023-01-13 22:20:13 +0500

    User update updated

--- src/Caravan.Service/Dtos/Users/UserUpdateDto.cs
+++ src/Caravan.Service/Dtos/Users/UserUpdateDto.cs
@@ -11,18 +11,13 @@ namespace Caravan.Service.Dtos.Users
 {
     public class UserUpdateDto
     {
-        [Required, MaxLength(25), MinLength(3)]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required, MaxLength(25), MinLength(3)]
         public string LastName { get; set; } = string.Empty;
 
         public string Address { get; set; } = string.Empty;
 
-        [Required, PhoneNumberAttribute]
+        [PhoneNumberAttribute]
         public string PhoneNumber { get; set; } = string.Empty;
-
-        [Required, EmailAttribute]
-        public string Email { get; set; } = string.Empty;
     }
 }
