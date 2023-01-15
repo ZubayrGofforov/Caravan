6be0509ba4c567fedf921a8e7f6f6ab0ff3721c2
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-01-15 18:06:57 +0500

    update view model

--- src/Caravan.Service/ViewModels/LocationViewModel.cs
+++ src/Caravan.Service/ViewModels/LocationViewModel.cs
@@ -8,6 +8,7 @@ namespace Caravan.Service.ViewModels
 {
     public class LocationViewModel
     {
+        public long Id { get; set; }
         public double Longitude { get; set; }
 
         public double Latitude { get; set; }
