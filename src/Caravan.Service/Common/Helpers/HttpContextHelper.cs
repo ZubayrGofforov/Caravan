b26125c1bd23b54faba9e924cd386026466c1cff
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-01-15 19:15:56 +0500

    Update Works

--- src/Caravan.Service/Common/Helpers/HttpContextHelper.cs
+++ src/Caravan.Service/Common/Helpers/HttpContextHelper.cs
@@ -19,7 +19,7 @@ public class HttpContextHelper
     public static HttpContext HttpContext => Accessor?.HttpContext;
     public static long UserId => GetUserId();
 
-    public static string UserRole => HttpContext?.User.FindFirst("UserRole")?.Value;
+    public static string UserRole => HttpContext?.User.FindFirst("http://schemas.microsoft.com/ws/2008/06/identity/claims/role")?.Value;
     private static long GetUserId()
     {
         long id;
